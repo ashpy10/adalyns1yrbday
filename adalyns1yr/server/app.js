2c3f8de0abf9b24fc0f1d73d147398bf7f4699df
Ashpy10 <pyka.ashley@gmail.com>
2025-07-15 13:39:22 -0700

    Update CORS & hid env file

--- adalyns1yr/server/app.js
+++ adalyns1yr/server/app.js
@@ -6,7 +6,11 @@ const app = express();
 
 // Enable CORS for frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173', 
+    'http://127.0.0.1:5173',
+    'https://adalynbday.netlify.app'
+  ],
   credentials: true
 }));
 
